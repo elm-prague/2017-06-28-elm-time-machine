5375a8a207e4f1a2517befff098bfb7e54a9523f
Adam Kučera <kucera@cngroup.dk>
2017-06-20 13:23:46 +0200

    Radio buttons implemented.
    - the buttons are moved down because of some zindex issues, we dont want to care about that

--- src/TimeControl.elm
+++ src/TimeControl.elm
@@ -1,6 +1,8 @@
 module TimeControl exposing (Model, init, Msg(..), update, view)
 
-import Html exposing (Html, div, text, h1, p)
+import Html exposing (Html, div, text, h1, p, input, label, select, fieldset, br)
+import Html.Attributes exposing (value, type_, checked, class)
+import Html.Events exposing (onClick)
 import Routes exposing (linkTo, Route(UnexploredRealityPage))
 import DatePicker exposing (DatePicker, defaultSettings, DateEvent(..))
 import Date exposing (Date)
@@ -10,16 +12,23 @@ import Date exposing (Date)
 type alias Model =
     { datePicker: DatePicker.DatePicker
     , pickedDate: Maybe Date
+    , color: Color
     }
 
 
 type Msg =
     DefaultMsg
     | SetDatePicker DatePicker.Msg
+    | SelectColor Color
+
+type Color =
+    Red
+    | Blue
+    | Green
 
 init : DatePicker -> Model
 init picker =
-    Model picker Nothing
+    Model picker Nothing Red
 
 
 --- UPDATE ---
@@ -49,6 +58,11 @@ update msg model =
                     }
                         ! [ Cmd.map SetDatePicker datePickerCmd ]
 
+            SelectColor color ->
+                { model
+                | color = color
+                } ! []
+
 --- VIEW ---
 
 view : Model -> Html Msg
@@ -62,9 +76,45 @@ view model =
                   defaultSettings
                   model.datePicker
                |> Html.map SetDatePicker
+            , div [class "bot-pad"] [ text "" ]
+            , text "There are three switches with different colours, which one will you select?"
             , p [] []
-            , text "parallel reality might be chosen randomly or by radio buttons"
+            , viewOptions model
             , p [] []
-            , Routes.linkTo (Routes.UnexploredRealityPage "Type of reality")
+            , Routes.linkTo (Routes.UnexploredRealityPage <| printColor model.color)
                 [] [ text "No fear" ]
             ]
+
+viewOptions: Model -> Html Msg
+viewOptions model =
+     fieldset []
+            [ radio Red model.color
+            , radio Blue model.color
+            , radio Green model.color
+            ]
+
+radio : Color -> Color -> Html Msg
+radio radioColor selectedColor =
+  let
+    isSelected =
+        if radioColor == selectedColor then
+            checked True
+        else
+            checked False
+  in
+  div []
+    [ label []
+        [ input [ type_ "radio", onClick (SelectColor radioColor), isSelected ] []
+        , text (printColor radioColor)
+        ]
+    ]
+
+printColor : Color -> String
+printColor color =
+    case color of
+        Red ->
+            "Red"
+        Green ->
+            "Green"
+        Blue ->
+            "Blue"
